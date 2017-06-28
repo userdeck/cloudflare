16eee45334579955a31e5aff79c1e4a906da71f2
Derek Myers <arcticpro@gmail.com>
2017-06-28 18:42:33 -0500

    test

--- source/app.js
+++ source/app.js
@@ -1,7 +1,33 @@
-(function () {
-  var options = INSTALL_OPTIONS;
-
-  if (!options.key) return;
-
-  // guides widget initializer
-}())
+(function() {
+  	'use strict'
+  
+  	if (!window.addEventListener) return;
+  
+ 	var options = INSTALL_OPTIONS;
+ 	var guide_key = options.guide_key;
+	var location = options.location;
+	
+	// console.log('here');
+	// console.log(options);
+ 	
+	if (!guide_key) return;
+	if (!location || !location.selector) return;
+  
+  	var el = INSTALL.createElement(location);
+	el.innerHTML = '<a href="http://userdeck.com" id="ud-guides">Customer Support Software</a>';
+	
+	// console.log(el);
+	
+	function updateElement () {
+		UserDeck.factory('guides', {
+			key: guide_key,
+			el: el
+		});
+  	}
+  
+  if (document.readyState == 'loading') {
+    window.addEventListener('DOMContentLoaded', updateElement)
+  } else {
+    // updateElement()
+  }
+})();
