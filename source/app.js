2e711dc9817663073f30871357b15e7a5e2ef034
Derek Myers <arcticpro@gmail.com>
2017-06-28 23:01:08 -0500

    fixes

--- source/app.js
+++ source/app.js
@@ -1,33 +1,36 @@
 (function() {
-  	'use strict'
-  
   	if (!window.addEventListener) return;
   
  	var options = INSTALL_OPTIONS;
  	var guide_key = options.guide_key;
 	var location = options.location;
-	
-	// console.log('here');
-	// console.log(options);
  	
 	if (!guide_key) return;
 	if (!location || !location.selector) return;
-  
-  	var el = INSTALL.createElement(location);
-	el.innerHTML = '<a href="http://userdeck.com" id="ud-guides">Customer Support Software</a>';
-	
-	// console.log(el);
 	
 	function updateElement () {
-		UserDeck.factory('guides', {
-			key: guide_key,
-			el: el
-		});
+	  	var element = INSTALL.createElement(location);
+		element.innerHTML = '<a href="http://userdeck.com" id="ud-guides">Customer Support Software</a>';
+
+	    if (!element || !element.parentNode) return;
+	    
+	    var s = document.createElement('script');
+	    s.type = 'text/javascript';
+	    // s.async = true;
+	    s.src = '//widgets.userdeck.com/guides.js';
+	    s.onload = function() {
+	    	UserDeck.factory('guides', {
+				key: guide_key,
+				el: element
+			});
+	    };
+	    
+	    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(s);
   	}
   
   if (document.readyState == 'loading') {
-    window.addEventListener('DOMContentLoaded', updateElement)
+    window.addEventListener('DOMContentLoaded', updateElement);
   } else {
-    // updateElement()
+    updateElement();
   }
 })();
--- source/guides.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    var s = document.createElement('script');
-    s.type = 'text/javascript';
-    // s.async = true;
-    s.src = '//widgets.userdeck.com/guides.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(s);
-})();
